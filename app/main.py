50e8bc39d1c3ef305fcc04ec8ed7772929008f6d
João Paulo Nogueira <joaonogueira@fisica.ufc.br>
2022-01-24 22:44:26 -0300

    change name from predict to prediction

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.database import engine
-from app.routers import user, predict
+from app.routers import prediction, user
 from app import models
 
 models.Base.metadata.create_all(bind=engine)
@@ -8,6 +8,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(user.router)
-app.include_router(predict.router)
+app.include_router(prediction.router)
 
 
